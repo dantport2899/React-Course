c70f48ef0404609cf74d2237bb94e36628d28912
dantport2899 <dantport28@gmail.com>
2022-04-27 10:18:48 -0500

    animated

---
+++ 06-RNComponents/src/screens/Animation101Screen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native';
+
+export const Animation101Screen = () => {
+  return (
+    <View style={{flex:1}}>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    purpleBoxL:{
+        backgroundColor:'purple',
+        width:150,
+        height:150,
+    }
+});
